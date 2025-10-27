6fec7eaba1f0c96758f5e2f36eeb7869038d12c6
Amirhossein Kakavand <kakavand2k@gmail.com>
2025-10-27 11:27:48 +0330

    Add storage URL to LINKS configuration

--- src/config/Links.ts
+++ src/config/Links.ts
@@ -1,6 +1,7 @@
 const LINKS = {
   personalWebsite: "https://akakavand.ir",
   github: "https://github.com/kaakavand/frontend-hiking-task",
+  storage_url: "https://hiking.storage.c2.liara.space/",
 };
 
 export default LINKS;
