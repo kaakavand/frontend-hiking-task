155aed267538b5dc3978e65368349b26f73fab8f
Amirhossein Kakavand <kakavand2k@gmail.com>
2025-10-27 09:03:48 +0330

    Add gallery page and middleware for redirection

---
+++ src/app/gallery/[id]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Modal from "@/components/ModalComponent";
+
+function Page() {
+  const router = useRouter();
+
+  return <Modal onClose={() => router.push("/")} />;
+}
+
+export default Page;
---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  if (pathname === "/gallery") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/gallery"],
+};
