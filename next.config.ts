dcfb0043a3ae323de184da8ddabd89310401f7c3
Amirhossein Kakavand <kakavand2k@gmail.com>
2025-10-27 11:27:32 +0330

    Configure Next.js to allow images from the hiking storage URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hiking.storage.c2.liara.space",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
